de07a430f4605d8b2638bbfd5ad4ccd21c306c3b
JordanHaer <jordanhaer@gmail.com>
2024-07-07 18:56:23 +0100

    Modified build script to be able build dependencies as xcframeworks.

--- build.sh
+++ build.sh
@@ -1,60 +1,69 @@
 #!/bin/bash
 
-set -x
 set -e
 
-# Pass scheme name as the first argument to the script
-NAME=$1
+# Check if Frameworks folder exists and remove any existing XCFrameworks
+if [ Frameworks ]; then
+    rm -rf Frameworks
+fi
+
+# Create Frameworks folder to store XCFrameworks in
+mkdir Frameworks
+
+# List packages that are to be converted to XCFrameworks
+for PACKAGE in "jLocation" "jNetworking"; do
 
 # Build the scheme for all platforms that we plan to support
 for PLATFORM in "iOS" "iOS Simulator"; do
 
     case $PLATFORM in
-    "iOS")
-    RELEASE_FOLDER="Release-iphoneos"
-    ;;
-    "iOS Simulator")
-    RELEASE_FOLDER="Release-iphonesimulator"
-    ;;
+        "iOS") RELEASE_FOLDER="Release-iphoneos" ;;
+        "iOS Simulator") RELEASE_FOLDER="Release-iphonesimulator" ;;
     esac
 
     ARCHIVE_PATH=$RELEASE_FOLDER
 
-    xcodebuild archive -workspace . -scheme $NAME \
-            -destination "generic/platform=$PLATFORM" \
-            -archivePath $ARCHIVE_PATH \
-            -derivedDataPath ".build" \
-            SKIP_INSTALL=NO \
-            BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+    xcodebuild archive -workspace . -scheme $PACKAGE \
+        -destination "generic/platform=$PLATFORM" \
+        -archivePath $ARCHIVE_PATH \
+        -derivedDataPath ".build" \
+        SKIP_INSTALL=NO \
+        BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
-    FRAMEWORK_PATH="$ARCHIVE_PATH.xcarchive/Products/usr/local/lib/$NAME.framework"
+    FRAMEWORK_PATH="$ARCHIVE_PATH.xcarchive/Products/usr/local/lib/$PACKAGE.framework"
+
     MODULES_PATH="$FRAMEWORK_PATH/Modules"
+
     mkdir -p $MODULES_PATH
 
-    BUILD_PRODUCTS_PATH=".build/Build/Intermediates.noindex/ArchiveIntermediates/$NAME/BuildProductsPath"
-    RELEASE_PATH="$BUILD_PRODUCTS_PATH/$RELEASE_FOLDER"
-    SWIFT_MODULE_PATH="$RELEASE_PATH/$NAME.swiftmodule"
-    RESOURCES_BUNDLE_PATH="$RELEASE_PATH/${NAME}_${NAME}.bundle"
+    BUILD_PRODUCTS_PATH=".build/Build/Intermediates.noindex/ArchiveIntermediates/$PACKAGE/BuildProductsPath"
 
-    # Copy Swift modules
-    if [ -d $SWIFT_MODULE_PATH ]
-    then
+    RELEASE_PATH="$BUILD_PRODUCTS_PATH/$RELEASE_FOLDER"
+
+    SWIFT_MODULE_PATH="$RELEASE_PATH/$PACKAGE.swiftmodule"
+
+    RESOURCES_BUNDLE_PATH="$RELEASE_PATH/${PACKAGE}_${PACKAGE}.bundle"
+
+    if [ -d $SWIFT_MODULE_PATH ]; then
         cp -r $SWIFT_MODULE_PATH $MODULES_PATH
     else
-        # In case there are no modules, assume C/ObjC library and create module map
-        echo "module $NAME { export * }" > $MODULES_PATH/module.modulemap
-        # TODO: Copy headers
+        echo "module $PACKAGE { export * }" > $MODULES_PATH/module.modulemap
     fi
 
     # Copy resources bundle, if exists
-    if [ -e $RESOURCES_BUNDLE_PATH ]
-    then
+    if [ -e $RESOURCES_BUNDLE_PATH ]; then
         cp -r $RESOURCES_BUNDLE_PATH $FRAMEWORK_PATH
     fi
 
 done
 
 xcodebuild -create-xcframework \
--framework Release-iphoneos.xcarchive/Products/usr/local/lib/$NAME.framework \
--framework Release-iphonesimulator.xcarchive/Products/usr/local/lib/$NAME.framework \
--output $NAME.xcframework
+    -framework Release-iphoneos.xcarchive/Products/usr/local/lib/$PACKAGE.framework \
+    -framework Release-iphonesimulator.xcarchive/Products/usr/local/lib/$PACKAGE.framework \
+    -output ./Frameworks/$PACKAGE.xcframework
+
+rm -rf Release-iphoneos.xcarchive
+rm -rf Release-iphonesimulator.xcarchive
+
+done
+
